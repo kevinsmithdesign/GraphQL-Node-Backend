4d6a25ead6e8f327fe8ef98cc9d838dd1155e6a5
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2018-08-12 15:57:25 -0500

    Setting up Schema

--- schema.js
+++ schema.js
@@ -7,6 +7,51 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
+// Customer Type 
+const CustomerType = new GraphQLObjectType({
+    name: 'Customer',
+    fields:() => ({
+        id: {type:GraphQLString},
+        name: {type: GraphQLString},
+        email: {type:GraphQLString},
+        age: {type: GraphQLInt}
+    })
+});
+
+// Hardcoded Data
+const customers = [
+    {id:'1', name: 'John Doe', email: 'jdoe@gmail.com', age:35},
+    {id:'2', name: 'Steve Smith', email: 'ssmith@gmail.com', age:25},
+    {id:'3', name: 'Sara Williams', email: 'swilliams@gmail.com', age:36}
+
+];
+
+//Root Query 
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: {type:GraphQLString}
+            },
+            resolve(parentValue, args){
+                for(let i = 0; i < customers.length;i++) {
+                    if(customers[i].id === args.id) {
+                        return customers[i];
+                    }
+                }
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args){
+                return customers;
+            }
+        }
+    }
+});
+
 module.exports = new GraphQLSchema({
-    
+    query: RootQuery
 });
